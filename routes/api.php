6c049f012cb4f157a40cce3e2614f69065cdf2be
Gustavo12386 <gucalderaro@hotmail.com>
2024-08-08 21:37:15 -0300

    adicionando metodos de atualizar e deletar

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,11 @@ use App\Http\Controllers\Api\V1\InvoiceController;
 Route::prefix('v1')->group(function(){
   Route::get('/users', [UserController::class, 'index']);
   Route::get('/users/{user}', [UserController::class, 'show']);    
-  Route::get('/invoices', [InvoiceController::class, 'index']);
-  Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']); 
-  Route::post('/invoices', [InvoiceController::class, 'store']);
-  Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
-  Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
+  Route::apiResource('invoices', InvoiceController::class);
+//  Route::get('/invoices', [InvoiceController::class, 'index']);
+//  Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']); 
+//  Route::post('/invoices', [InvoiceController::class, 'store']);
+//  Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
+//  Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
 });
   
